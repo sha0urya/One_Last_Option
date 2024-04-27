3dace69e447bb968c15ca98512e6ab12dbbb6f16
Praveen Kumar <singhsatyam312005@gmail.com>
2024-04-28 02:54:37 +0530

    add delete user functionality to the admin user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,11 +1,9 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
-
 export const test = (req, res) => {
   res.json({ message: 'API is working!' });
 };
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -55,7 +53,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
   try {
@@ -65,7 +63,6 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
-
 export const signout = (req, res, next) => {
   try {
     res
@@ -76,7 +73,6 @@ export const signout = (req, res, next) => {
     next(error);
   }
 };
-
 export const getUsers = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to see all users'));
@@ -85,21 +81,16 @@ export const getUsers = async (req, res, next) => {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.sort === 'asc' ? 1 : -1;
-
     const users = await User.find()
       .sort({ createdAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
-
     const usersWithoutPassword = users.map((user) => {
       const { password, ...rest } = user._doc;
       return rest;
     });
-
     const totalUsers = await User.countDocuments();
-
     const now = new Date();
-
     const oneMonthAgo = new Date(
       now.getFullYear(),
       now.getMonth() - 1,
@@ -108,7 +99,6 @@ export const getUsers = async (req, res, next) => {
     const lastMonthUsers = await User.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
-
     res.status(200).json({
       users: usersWithoutPassword,
       totalUsers,
